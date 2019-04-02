a193e6ea1972af75b3a98525aa61f0950784a2f1
lv chenghui <fengxiangpiao@hotmail.com>
2019-04-02 15:51:19 +0800

    add main file“

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-import brickstate
 
 '''
 正面，侧面，顶面摄像头
 '''
 
-###获取摄像头数据,计算某个面偏转角度
+def getFaceRotate( faceIndex ):
+    rotate = 0
+    if faceIndex == 0 :
+        rotate = 0.1
+    elif faceIndex == 1 :
+        rotate = 0.2
+    elif faceIndex == 2 :
+        rotate = 0.3
+    return rotate
 
+
+###获取摄像头数据,计算某个面偏转角度
+faceFront = getFaceRotate(0)
 ###根据偏转角度,调用硬件调整姿态
+'''
+升降脚
+前:左(FL),右(FR);后:左(BL)右(BR)
+'''
+footFrontleft = "FL" 
+footFrontRight = "FR"
+footBackLeft = "BL"
+footBackRight = "BR"
+
+
+
