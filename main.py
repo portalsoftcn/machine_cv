9dfb00d1112187bda3fa2539126a2ad82e4a979c
lvchenghui <fengxiangpiao@hotmail.com>
2019-04-06 16:59:36 +0800

    modify showimg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from machine import Arduino
 from camerastate import CameraState
 from matplotlib import pyplot as plt
@@ -25,19 +26,25 @@ frontCamera = CameraState(CameraState.frontFace)
 rightCamera = CameraState(CameraState.rightFace)
 
 while True:
-    cv2.waitKey(25)
     
     frontRotate,frontFrame = frontCamera.getFaceState()
+    cv2.waitKey(20)
     rightRotate,rightFrame = rightCamera.getFaceState()
-
+    cv2.waitKey(20)
+    
+    frame = np.hstack( (frontFrame,rightFrame) )
+            
+    cv2.imshow("Front",frame)
+    
+    '''
     plt.subplot(121),plt.imshow(frontFrame)
     plt.title("Front"),plt.xticks([]),plt.yticks([])
 
     plt.subplot(122),plt.imshow(rightFrame)
     plt.title("Right"),plt.xticks([]),plt.yticks([])
-    plt.show()
-
-    '''
+    
+    plt.show() 
+    
     text = 'rotate:%.1f'%frontRotate
     org = 40,80
     fontFace = cv2.FONT_HERSHEY_COMPLEX
@@ -47,9 +54,12 @@ while True:
     lintType = 4
     bottomLeftOrigin = 1
     cv2.putText(frontFrame,text,org,fontFace,fontScale,fontColor,thickness,lintType)
-    cv2.imshow(faceType,frontFrame)
+    
+    
+    cv2.imshow("Right",rightFrame)
     '''
     
+
 
 
 
