2b23269a68399035e4c0ee4baf1905f560526b4f
lv chenghui <fengxiangpiao@hotmail.com>
2019-04-07 09:34:10 +0800

    modify get face

--- brickface.py
+++ brickface.py
@@ -41,7 +41,7 @@ class BrickFace:
             x,y,w,h = cv2.boundingRect(maxContours)    
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
 
-            text = 'rotate:%.1f'%rotate
+            text = format("%s rotate:%.1f",self.faceType,rotate)
             org = 40,80
             fontFace = cv2.FONT_HERSHEY_COMPLEX
             fontScale = 1
