fa556e273a1ef24d46f65b8b5dab26c0f72139b5
lvchenghui <fengxiangpiao@hotmail.com>
2019-06-05 15:51:57 +0800

    modify live

--- live.py
+++ live.py
@@ -15,6 +15,66 @@ kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(2, 2))
 
 lower_blue,upper_blue = hsvUtil.getFilteRange()
 
+#业务数据计算
+topCamera = None
+
+deviceIP = "192.168.1.8"
+deviceIP2 = "192.168.1.15"
+serverIP = "192.168.1.11"
+
+def getCamera(cameraUrl):
+    print(cameraUrl)
+    camera = cv2.VideoCapture(cameraUrl) # 从文件读取视频
+    #这里的摄像头可以在树莓派3b上使用
+    if (camera.isOpened()):# 判断视频是否打开 
+        print ('Open camera')
+    else:
+        print ('Fail to open camera!')
+    return camera
+
+def getRtmpPipe(camera,rtmpUrl):
+    # 视频属性
+    size = (int(camera.get(cv2.CAP_PROP_FRAME_WIDTH)), int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    sizeStr = str(size[0]) + 'x' + str(size[1])
+    fps = camera.get(cv2.CAP_PROP_FPS)  # 30p/self
+    fps = int(fps)
+    hz = int(1000.0 / fps)
+    print ('size:'+ sizeStr + ' fps:' + str(fps) + ' hz:' + str(hz))
+    #sizeStr = '600x450'
+    #fps=20
+    # 直播管道输出 
+    # ffmpeg推送rtmp 重点 ： 通过管道 共享数据的方式
+    frontCommand = ['ffmpeg',
+        '-y',
+        '-f', 'rawvideo',
+        '-vcodec','rawvideo',
+        '-pix_fmt', 'bgr24',
+        '-s', sizeStr,
+        '-r', str(fps),
+        '-i', '-',
+        '-c:v', 'libx264',
+        '-pix_fmt', 'yuv420p',
+        '-preset', 'ultrafast',
+        '-f', 'flv', 
+        rtmpUrl]
+    #管道特性配置
+    pipe = sp.Popen(frontCommand, stdin=sp.PIPE) #,shell=False
+    return pipe
+
+frontCamera = getCamera("http://"+deviceIP+":8001/?action=stream")
+frontPipe = getRtmpPipe(frontCamera,'rtmp://'+serverIP+':1931/device/front1')
+
+backCamera = getCamera("http://"+deviceIP+":8003/?action=stream")
+backPipe = getRtmpPipe(backCamera,'rtmp://'+serverIP+':1931/device/back1')
+
+leftCamera = getCamera("http://"+deviceIP2+":8001/?action=stream")
+leftPipe = getRtmpPipe(leftCamera,'rtmp://'+serverIP+':1931/device/left1')
+
+rightCamera = getCamera("http://"+deviceIP2+":8003/?action=stream")
+rightPipe = getRtmpPipe(rightCamera,'rtmp://'+serverIP+':1931/device/right1')
+
+topPipe = getRtmpPipe(rightCamera,'rtmp://'+serverIP+':1931/device/top1')
+
 def processImg(frame,diff):
     imgGray = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)[1]
     imgGray = cv2.dilate(imgGray, es, iterations=2)
@@ -41,45 +101,6 @@ def processImg(frame,diff):
     textUtil.putText(frame,text)
     return frame
 
-def getCamera(cameraUrl):
-    print(cameraUrl)
-    camera = cv2.VideoCapture(cameraUrl) # 从文件读取视频
-    #这里的摄像头可以在树莓派3b上使用
-    if (camera.isOpened()):# 判断视频是否打开 
-        print ('Open camera')
-    else:
-        print ('Fail to open camera!')
-    return camera
-
-def getRtmpPipe(camera,rtmpUrl):
-    # 视频属性
-    size = (int(camera.get(cv2.CAP_PROP_FRAME_WIDTH)), int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    sizeStr = str(size[0]) + 'x' + str(size[1])
-    fps = camera.get(cv2.CAP_PROP_FPS)  # 30p/self
-    fps = int(fps)
-    hz = int(1000.0 / fps)
-    print ('size:'+ sizeStr + ' fps:' + str(fps) + ' hz:' + str(hz))
-    #sizeStr = '600x450'
-
-    # 直播管道输出 
-    # ffmpeg推送rtmp 重点 ： 通过管道 共享数据的方式
-    frontCommand = ['ffmpeg',
-        '-y',
-        '-f', 'rawvideo',
-        '-vcodec','rawvideo',
-        '-pix_fmt', 'bgr24',
-        '-s', sizeStr,
-        '-r', str(fps),
-        '-i', '-',
-        '-c:v', 'libx264',
-        '-pix_fmt', 'yuv420p',
-        '-preset', 'ultrafast',
-        '-f', 'flv', 
-        rtmpUrl]
-    #管道特性配置
-    pipe = sp.Popen(frontCommand, stdin=sp.PIPE) #,shell=False
-    return pipe
-
 def getRtmpFrame(camera):
     ###########################图片采集
     ret, frame = camera.read() # 逐帧采集视频流
@@ -88,53 +109,33 @@ def getRtmpFrame(camera):
     return frame
 
 def pushRtmp():
-    frontFrame = getRtmpFrame(frontCamera)
-    #ret1,frontFrame = frontCamera.read() 
+    #frontFrame = getRtmpFrame(frontCamera)
+    ret1,frontFrame = frontCamera.read() 
     frontPipe.stdin.write(frontFrame.tostring())  
     
-    backFrame = getRtmpFrame(backCamera)
-    #ret2,backFrame = backCamera.read() 
+    #backFrame = getRtmpFrame(backCamera)
+    ret2,backFrame = backCamera.read() 
     backPipe.stdin.write(backFrame.tostring())  
     
     #leftFrame = getRtmpFrame(leftCamera)
-    #ret3,leftFrame = leftCamera.read() 
-    leftPipe.stdin.write(backFrame.tostring())  
+    ret3,leftFrame = leftCamera.read() 
+    leftPipe.stdin.write(leftFrame.tostring())  
 
     #leftFrame = getRtmpFrame(leftCamera)
-    topPipe.stdin.write(backFrame.tostring())  
+    ret4,rightFrame = rightCamera.read() 
+    rightPipe.stdin.write(rightFrame.tostring())  
 
-    #leftFrame = getRtmpFrame(leftCamera)
-    rightPipe.stdin.write(backFrame.tostring())  
-
-#业务数据计算
-frontCamera = None
-leftCamera = None
-topCamera = None
-rightCamera = None
-backCamera = None
-
-deviceIP = "192.168.1.8"
-serverIP = "192.168.1.11"
-
-frontCamera = getCamera("http://"+deviceIP+":8001/?action=stream")
-frontPipe = getRtmpPipe(frontCamera,'rtmp://'+serverIP+':1931/device/front1')
-
-backCamera = getCamera("http://"+deviceIP+":8003/?action=stream")
-backPipe = getRtmpPipe(backCamera,'rtmp://'+serverIP+':1931/device/back1')
-
-#leftCamera = getCamera("http://"+deviceIP+":8004/?action=stream")
-leftPipe = getRtmpPipe(backCamera,'rtmp://'+serverIP+':1931/device/left1')
-
-topPipe = getRtmpPipe(backCamera,'rtmp://'+serverIP+':1931/device/top1')
-
-rightPipe = getRtmpPipe(backCamera,'rtmp://'+serverIP+':1931/device/right1')
+    #topFrame = getRtmpFrame(topCamera)
+    topPipe.stdin.write(rightFrame.tostring())  
 
 def cameraRelease():
     frontCamera.release()
     backCamera.release()
-    #leftCamera.release()
+    leftCamera.release()
+    rightCamera.release()
     #topCamera.release()
-    #rightCamera.release()
+
+
 
 while True:
     pushRtmp()
