cd837a2d3ee3c07ebec324f9eeb395d81c2101f5
lvchenghui <fengxiangpiao@hotmail.com>
2019-06-15 09:47:03 +0800

    modify ip

--- live.py
+++ live.py
@@ -17,9 +17,8 @@ lower_blue,upper_blue = hsvUtil.getFilteRange()
 
 #业务数据计算
 
-deviceIP = "192.168.1.14"
-deviceIP2 = "192.168.1.14"
-serverIP = "192.168.1.12"
+deviceIP = "192.168.1.9"
+serverIP = "192.168.1.11"
 
 def getCamera(cameraUrl):
     print(cameraUrl)
@@ -36,7 +35,6 @@ def getRtmpPipe(camera,rtmpUrl):
     size = (int(camera.get(cv2.CAP_PROP_FRAME_WIDTH)), int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     sizeStr = str(size[0]) + 'x' + str(size[1])
     fps = camera.get(cv2.CAP_PROP_FPS)  # 30p/self
-    fps=25
     fps = int(fps)
     hz = int(1000.0 / fps)
     print ('size:'+ sizeStr + ' fps:' + str(fps) + ' hz:' + str(hz))
@@ -46,7 +44,6 @@ def getRtmpPipe(camera,rtmpUrl):
     # ffmpeg推送rtmp 重点 ： 通过管道 共享数据的方式
     frontCommand = ['ffmpeg',
         '-y',
-        '-hwaccel','qsv',
         '-f', 'rawvideo',
         '-vcodec','rawvideo',
         '-pix_fmt', 'bgr24',
@@ -70,15 +67,15 @@ backCamera = getCamera("http://"+deviceIP+":8003/?action=stream")
 #backCamera = getCamera(1)
 backPipe = getRtmpPipe(backCamera,'rtmp://'+serverIP+':1931/device/back1')
 
-leftCamera = getCamera("http://"+deviceIP2+":8005/?action=stream")
+leftCamera = getCamera("http://"+deviceIP+":8005/?action=stream")
 #leftCamera = getCamera(2)
 leftPipe = getRtmpPipe(leftCamera,'rtmp://'+serverIP+':1931/device/left1')
 
-rightCamera = getCamera("http://"+deviceIP2+":8007/?action=stream")
+rightCamera = getCamera("http://"+deviceIP+":8007/?action=stream")
 #rightCamera = getCamera(2)
 rightPipe = getRtmpPipe(rightCamera,'rtmp://'+serverIP+':1931/device/right1')
 
-topCamera = getCamera("http://"+deviceIP2+":8009/?action=stream")
+topCamera = getCamera("http://"+deviceIP+":8009/?action=stream")
 #topCamera = getCamera(2)
 topPipe = getRtmpPipe(topCamera,'rtmp://'+serverIP+':1931/device/top1')
 
