85f76c1a4d29d7faeb0911dcb362a0f214006416
lvchenghui <fengxiangpiao@hotmail.com>
2019-06-23 16:51:26 +0800

    modify open camera

--- multi_camera.sh
+++ multi_camera.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-cd /home/pi/mjpg-streamer-master/mjpg-streamer-experimental/
-nohup ./mjpg_streamer -i "input_uvc.so -y -d /dev/video0 -r 640x480 -f 30" -o "output_http.so -w ./www -p 8001" &
-nohup ./mjpg_streamer -i "input_uvc.so -y -d /dev/video1 -r 640x480 -f 30" -o "output_http.so -w ./www -p 8002" &
-nohup ./mjpg_streamer -i "input_uvc.so -y -d /dev/video2 -r 640x480 -f 30" -o "output_http.so -w ./www -p 8003" &
-nohup ./mjpg_streamer -i "input_uvc.so -y -d /dev/video3 -r 640x480 -f 30" -o "output_http.so -w ./www -p 8004" &
-nohup ./mjpg_streamer -i "input_uvc.so -y -d /dev/video4 -r 640x480 -f 30" -o "output_http.so -w ./www -p 8005" &
-nohup ./mjpg_streamer -i "input_uvc.so -y -d /dev/video5 -r 640x480 -f 30" -o "output_http.so -w ./www -p 8006" &
+cd /home/pi/machine_cv/mjpg-streamer-master/mjpg-streamer-experimental/
+nohup mjpg_streamer -i "input_uvc.so -d /dev/video0 -r 640x480 " -o "output_http.so -w ./www -p 8001" &
+nohup mjpg_streamer -i "input_uvc.so -d /dev/video2 -r 640x480 " -o "output_http.so -w ./www -p 8003" &
+nohup mjpg_streamer -i "input_uvc.so -d /dev/video4 -r 640x480 " -o "output_http.so -w ./www -p 8005" &
+nohup mjpg_streamer -i "input_uvc.so -d /dev/video6 -r 640x480 " -o "output_http.so -w ./www -p 8007" &
+nohup mjpg_streamer -i "input_uvc.so -d /dev/video8 -r 640x480 " -o "output_http.so -w ./www -p 8009" &
 
