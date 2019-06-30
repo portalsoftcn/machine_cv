4d2a1ea5f0576c22c9e867cf3af9743a802361ee
lvchenghui <fengxiangpiao@hotmail.com>
2019-06-30 10:38:25 +0800

    add openserver

---
+++ openserver.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /usr/local/src/nginx/sbin
+sudo ./nginx
+nohup mjpg_streamer -i "input_uvc.so -d /dev/video0 -r 640x480 " -o "output_http.so -w ./www -p 8001"&
+nohup mjpg_streamer -i "input_uvc.so -d /dev/video2 -r 640x480 " -o "output_http.so -w ./www -p 8003"
