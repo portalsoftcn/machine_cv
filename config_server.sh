592a7cdab9c72c6161ed64b84a5478686538a044
风想飘 <fengxiangpiao@hotmail.com>
2019-07-04 23:48:55 +0800

    modify config

--- config_server.sh
+++ config_server.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-yum update
+yum update -y
 yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite sqlite-devel 
 yum install -y readline-devel tk tk-devel gdbm gdbm-devel db4-devel libpcap-devel lzma xz xz-devel libuuid-devel libffi-devel
 yum install -y emacs
 tar -zxvf Python-3.7.3.tgz
 cd Python-3.7.3
 ./configure --prefix=/usr/local/python3
+make && make install
 ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3
 ln -s /usr/local/python3/bin/pip3 /usr/local/bin/pip3
 pip3 install pip --upgrade
