c0ce7877a2d648c4a6840950ceb74bfb41d12be7
风想飘 <fengxiangpiao@hotmail.com>
2019-07-04 23:43:22 +0800

    modify file

--- config_server.sh
+++ config_server.sh
@@ -2,7 +2,7 @@
 yum update
 yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite sqlite-devel 
 yum install -y readline-devel tk tk-devel gdbm gdbm-devel db4-devel libpcap-devel lzma xz xz-devel libuuid-devel libffi-devel
-yum install -y emacs git
+yum install -y emacs
 tar -zxvf Python-3.7.3.tgz
 cd Python-3.7.3
 ./configure --prefix=/usr/local/python3
