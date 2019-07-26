2fd1c79ae19056dcd52e95fab281dd132b5b09cc
lvchenghui <fengxiangpiao@hotmail.com>
2019-07-26 16:08:16 +0800

    modify config server

--- config_server.sh
+++ config_server.sh
@@ -13,12 +13,10 @@ ln -s /usr/local/python3/bin/pip3 /usr/local/bin/pip3
 pip3 install pip --upgrade
 pip3 install opencv-python
 pip3 install scipy
-cd /home/machine_cv
+cd ../machine_cv
 tar -zxvf nginx-1.17.1.tar.gz
 mv nginx-rtmp-module nginx-1.17.1
 cd nginx-1.17.1
 ./configure --prefix=/usr/local/nginx --add-module=nginx-rtmp-module  --with-http_ssl_module
 make && make install
-rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
-rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
-yum install ffmpeg ffmpeg-devel -y
+
