c1abcdadca3f62d769b7a1886ff2616c20077ab2
lvchenghui <fengxiangpiao@hotmail.com>
2019-07-27 15:59:43 +0800

    modify transface

--- transface.py
+++ transface.py
@@ -5,28 +5,35 @@ import getopt
 
 face = ""
 videourl = ""
+serverip = ""
 
 try:
-    opts,args = getopt.getopt(sys.argv[1:],"hf:v:",["face=","video="])
+    opts, args = getopt.getopt(sys.argv[1:], "-h-f:-v:-s:", ["face=", "video=","server="])
 except getopt.GetoptError:
-    print("transface.py  -f  <face> -v <video>")
+    print("transface.py  -f <face> -v <video>  -s<server>")
     sys.exit(2)
 
-for opt,arg in opts:
+for opt, arg in opts:
     if opt == '-h':
-        print("transface.py  -f  <face> -v <video>")
+        print("transface.py -f <face> -v<video> -s<server>")
         sys.exit()
-    elif opt in ("-f","--face"):
+    elif opt in ("-f", "--face"):
         face = arg
-    elif opt in ("-v","--video"):
+    elif opt in ("-v", "--video"):
         videourl = arg
+    elif opt in ("-s","--server"):
+        serverip = arg
 
-url = "http://192.168.1.18/upload.php"
+print(face)
+print(videourl)
+print(serverip)
+
+url = "http://"+serverip+"/upload.php"
 fileName = face+".jpg"
 camera = cv2.VideoCapture(videourl)
 while True:
-    ret,frame = camera.read()
-    cv2.imwrite(fileName,frame)
-    files = {"file":(fileName,open(fileName,"rb"),"image/jpg",{})}
-    res = requests.request("POST",url,data={"face":face},files=files)
+    ret, frame = camera.read()
+    cv2.imwrite(fileName, frame)
+    files = {"file": (fileName, open(fileName, "rb"), "image/jpg", {})}
+    res = requests.request("POST", url, data={"face": face}, files=files)
     cv2.waitKey(25)
