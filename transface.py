6f92b60157fb2d709e8a01419df84524317b1cc2
lvchenghui <fengxiangpiao@hotmail.com>
2019-07-30 11:08:35 +0800

    modify ip

--- transface.py
+++ transface.py
@@ -5,26 +5,24 @@ import getopt
 
 face = ""
 videourl = ""
-serverip = ""
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "-h-f:-v:-s:", ["face=", "video=","server="])
+    opts, args = getopt.getopt(sys.argv[1:], "-h-f:-v:", ["face=", "video="])
 except getopt.GetoptError:
-    print("transface.py  -f <face> -v <video>  -s<server>")
+    print("transface.py  -f <face> -v <video>")
     sys.exit(2)
 
 for opt, arg in opts:
     if opt == '-h':
-        print("transface.py -f <face> -v<video> -s<server>")
+        print("transface.py -f <face> -v<video>")
         sys.exit()
     elif opt in ("-f", "--face"):
         face = arg
     elif opt in ("-v", "--video"):
         videourl = arg
-    elif opt in ("-s","--server"):
-        serverip = arg
 
-url = "http://"+serverip+"/web/upload.php"
+
+url = "http://www.portalsoft.cn/upload.php"
 fileName = face+".jpg"
 camera = cv2.VideoCapture(videourl)
 while True:
@@ -33,4 +31,4 @@ while True:
     files = {"file": (fileName, open(fileName, "rb"), "image/jpg", {})}
     res = requests.request("POST", url, data={"face": face}, files=files)
     cv2.waitKey(25)
-    print(serverip + face + "  trans success")
+    print(face + "  trans success")
