9dae2d8e200d846f39a43dd60c6b8f1452b3ef92
lvchenghui <fengxiangpiao@hotmail.com>
2019-07-30 11:38:20 +0800

    modify upload.php

--- web/upload.php
+++ web/upload.php
@@ -1,9 +1,9 @@
 <?php
 $face = $_POST["face"];
 $uploadDir = "uploadimg/".$face."/";
-$faceFile = "faceimg/".$face."/1.jpg";
 $jpgFiles = glob($uploadDir."*.jpg");
 $fileCount = count($jpgFiles)+1;
 $uploadFile = $uploadDir . $fileCount.".jpg";
+echo $uploadFile;
 move_uploaded_file($_FILES["file"]["tmp_name"],$uploadFile);
 ?>
