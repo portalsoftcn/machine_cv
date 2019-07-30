e545d9d0c173d11907045b8eca280a86bb2378bf
lvchenghui <fengxiangpiao@hotmail.com>
2019-07-30 16:40:50 +0800

    modify config

--- config_server.sh
+++ config_server.sh
@@ -18,11 +18,6 @@ tar -zxvf nginx-1.17.1.tar.gz
 cd nginx-1.17.1
 ./configure --prefix=/usr/local/nginx  --with-http_ssl_module
 make && make install
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-yum install php72w.x86_64 php72w-fpm.x86_64 php72w-cli.x86_64 php72w-common.x86_64 php72w-gd.x86_64 php72w-ldap.x86_64 php72w-mbstring.x86_64 php72w-mcrypt.x86_64 php72w-mysqlnd.x86_64 php72w-pdo.x86_64 php72w-pecl-redis.x86_64 php72w-opcache.x86_64 php72w-devel.x86_64 php72w-bcmath.x86_64 -y
-systemctl start php-fpm
-systemctl enable php-fpm
 
 #mv /root/machine_cv /usr/local/nginx/html
 #cp /usr/local/nginx/html/nginx.conf /usr/local/nginx/conf
