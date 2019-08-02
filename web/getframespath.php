ba74c5d7fea4783b2576ee6daf6fed974e14e187
lvchenghui <fengxiangpiao@hotmail.com>
2019-08-02 09:06:50 +0800

    modify getframe

--- web/getframespath.php
+++ web/getframespath.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 function getFileAmount($face)
 {
     $faceDir = "faceimg/".$face."/";
@@ -7,13 +8,24 @@ function getFileAmount($face)
     $fileCount = count($jpgFiles);
     return $fileCount-1;
 }
+*/
 
 $facesAmount = [];
-$facesAmount[0] = getFileAmount("front");
-$facesAmount[1] = getFileAmount("back");
-$facesAmount[2] = getFileAmount("left");
-$facesAmount[3] = getFileAmount("right");
-$facesAmount[4] = getFileAmount("top");
+$result = "";
+$before = time();
+for($i = 0;$i<1000;$i++)
+{
+    $facesAmount[0] = getFileAmount("front");
+    $facesAmount[1] = getFileAmount("back");
+    $facesAmount[2] = getFileAmount("left");
+    $facesAmount[3] = getFileAmount("right");
+    $facesAmount[4] = getFileAmount("top");
+    $result = join(",",$facesAmount );
+}
+#echo $result;
 
-echo join(",",$facesAmount )
+$curr = time();
+
+echo $curr - $before;
+
 ?>
