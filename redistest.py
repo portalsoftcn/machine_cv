cf6904272faa5ed95b57b80c1cded8378c256213
风想飘 <fengxiangpiao@hotmail.com>
2019-08-10 09:07:47 +0800

    modify test

--- redistest.py
+++ redistest.py
@@ -1,11 +1,18 @@
 import redis
-import cv2
+import base64
 import time
+
 
 red = redis.Redis(host='localhost', port=6379)
 get_now_milli_time = lambda:int( time.time() * 1000 )
-while True:
+
+faceIndex = 1
+
+while faceIndex < 14939:
     read_before = get_now_milli_time()
-    count = red.get('count')
+    with open("web/faceimg/front/"+str(faceIndex)+".jpg","rb") as f:#转为二进制格式
+        base64_data = base64.b64encode(f.read())#使用base64进行加密
+        red.set("front/"+str(faceIndex)+".jpg",base64_data)
     read_after = get_now_milli_time()
-    print("count is:"+str(count)+" read use:"+str(read_after - read_before))
+    print(str(faceIndex)+" read use:"+str(read_after - read_before))
+    faceIndex = faceIndex + 1
