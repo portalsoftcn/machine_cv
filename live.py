a906735ef58423fad55c389cb80e44d7ddb67cd9
风想飘 <fengxiangpiao@hotmail.com>
2019-08-10 16:26:48 +0800

    modify live

--- live.py
+++ live.py
@@ -3,6 +3,8 @@ import cv2
 import subprocess as sp
 import time
 import requests
+import redis
+import base64
 from util import ContourUtil,TextUtil,ImgUtil,HSVFilteUtil
 from roi import ROIDetect
 
@@ -35,18 +37,21 @@ toppath = uploadPath+"top/"
 facePath = "web/faceimg/"
 
 faceCountArray = {"front":1,"back":1,"left":1,"right":1,"top":1}
-
+'''
 frontCamera = cv2.VideoCapture("http://device1.portalsoft.cn:8000/?action=stream")
 backCamera = cv2.VideoCapture("http://device1.portalsoft.cn:8002/?action=stream")
-
+'''
 leftCamera = cv2.VideoCapture("http://device2.portalsoft.cn:8000/?action=stream")
 rightCamera = cv2.VideoCapture("http://device2.portalsoft.cn:8002/?action=stream")
 
-count_url = "http://localhost/countfile.php?"
 faceAmount = 1
 
 get_now_milli_time = lambda:int( time.time() * 1000 )
 slowTimes = 0
+
+#pool = redis.ConnectionPool(host='localhost', port=6379)
+#red = redis.Redis(connection_pool=pool)
+red = redis.Redis(host='192.168.1.5', port=6379)
 
 def processImg(frame,diff,lostCount):
     imgGray = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)[1]
@@ -74,9 +79,9 @@ def processImg(frame,diff,lostCount):
     textUtil.putText(frame,text)
     return frame
 
-def getRtmpFrame(camera,lostCount):
+def getRtmpFrame(currFrame,lostCount):
     ###########################图片采集
-    ret, currFrame = camera.read() # 逐帧采集视频流
+    #ret, currFrame = camera.read() # 逐帧采集视频流
     diff = roiDetectFront.getROIByDiff(currFrame)
     frame = processImg(currFrame,diff,lostCount)
     return frame
@@ -86,45 +91,56 @@ def getFileAmount(dir):
     amount = len(files)
     return amount
 
+def storageImg(filePath):
+    with open(filePath,"rb") as f:#转为二进制格式
+        encode_before = get_now_milli_time()
+        base64_data = base64.b64encode(f.read())#使用base64进行加密
+        encode_after = get_now_milli_time()
+        red.set(filePath,base64_data)
+        storage_after = get_now_milli_time() 
+        os.remove(filePath)
+        print("storage encode use:"+str(encode_after - encode_before) + " db use:"+str(storage_after - encode_after))
+    
 def getAnalyseFrame(face,camera):
-    '''
+    
     read_before = get_now_milli_time()
     ret,currFrontFrame = camera.read()
     read_after = get_now_milli_time()
-    '''
-
+    
     analy_before = get_now_milli_time()
-    analyseFrame = getRtmpFrame(camera,0)
+    analyseFrame = getRtmpFrame(currFrontFrame,0)
     analy_after = get_now_milli_time()
 
     write_before = get_now_milli_time()
+    
     faceFile = facePath + face + "/"+str(faceAmount) + ".jpg"
     #print("faceFile:"+faceFile)
-    count_after = get_now_milli_time()
-
     writeImg = cv2.resize(analyseFrame,(400,300),interpolation=cv2.INTER_CUBIC) 
-
     cv2.imwrite(faceFile,writeImg)
     write_after = get_now_milli_time()
-    #cv2.imshow(face,analyseFrame)
+    storageImg(faceFile)
+    storage_after = get_now_milli_time()
+    #cv2.imshow(face,writeImg)
     #print("count use:"+str(count_after-write_after))
-    #print("per time read use:"+str(read_after - read_before) + " analy use:" +str(analy_after - analy_before)+ " write use:"+str(write_after - write_before) + " total use:"+str(write_after - read_before))
-    #print("  write detail count use:"+str(count_after - write_before)+ " write use:"+str(write_after - count_after) + " total use:"+str(write_after - write_before))
+    if (read_after - read_before) >= 10 :
+        print(face+" face read use:"+str(read_after - read_before)+" analy use:" +str(analy_after - analy_before)+ " write use:"+str(write_after - write_before) + " total use:"+str(write_after - read_before))
+    print("  write detail  save use:"+str(write_after - write_before) + " db use: "+str(storage_after - write_after)+" total use:"+str(storage_after - write_before))
+
 while True:
     read_before = get_now_milli_time()
+    '''
     getAnalyseFrame("front",frontCamera)
     getAnalyseFrame("back",backCamera)
+    '''
     getAnalyseFrame("left",leftCamera)
     getAnalyseFrame("right",rightCamera)
+    red.set("count",faceAmount)
     read_after = get_now_milli_time()
     fps = read_after - read_before
     fps = 1000 / fps
     fps = int(fps)
-    if fps <= 20 :
+    if fps <=15 :
         slowTimes = slowTimes + 1
-    #getAnalyseFrame("top")
     print("process use:"+str(read_after - read_before)+" fps:"+str(fps) + " slow time:"+str(slowTimes))
-    reqUrl = count_url+"count="+str(faceAmount)
-    requests.get(reqUrl)
     faceAmount = faceAmount + 1
-    cv2.waitKey(1)
+    
